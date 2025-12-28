ae09e957fb81e813d97a6a566bcac8b8af0bb627
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2025-12-28 18:29:18 +0100

    Create Spectateur.java
    Représente un spectateur identifié par son nom.

---
+++ Java/Gestion de Cinéma/src/Spectateur.java
@@ -0,0 +1,16 @@
+public class Spectateur {
+    public String nom;
+
+    public Spectateur(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Spectateur : " + nom;
+    }
+}
