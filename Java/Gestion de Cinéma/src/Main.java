71ebb93f3d22f3cd52880caf452c58ca3baf6309
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2025-12-28 18:30:48 +0100

    Create Main.java
    Classe de test qui crée un cinéma, ajoute plusieurs spectateurs et affiche le résultat.

---
+++ Java/Gestion de Cinéma/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cinema c = new Cinema(2);
+
+        Spectateur s1 = new Spectateur("Ali");
+        Spectateur s2 = new Spectateur("Karim");
+        Spectateur s3 = new Spectateur("Sara");
+        Spectateur s4 = new Spectateur("Nada");
+
+        System.out.println(c.placer(s1));
+        System.out.println(c.placer(s2));
+        System.out.println(c.placer(s3));
+        System.out.println(c.placer(s4));
+
+        System.out.println(c);
+    }
+}
