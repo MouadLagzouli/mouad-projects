1cd9507a2b0a1ac2571e14cd205076e2f556111e
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-01-02 17:42:49 +0100

    Create Recherche_dichotomique_récursive_croissant.c
    Version recursive avec bornes debut/fin

---
+++ C/Recherche_dichotomique_récursive_croissant.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int dicho_rec_croissant(const int t[], int debut, int fin, int val) {
+    if (debut > fin) return -1;
+
+    int m = debut + (fin - debut) / 2;
+
+    if (t[m] == val) return m;
+    if (val < t[m]) return dicho_rec_croissant(t, debut, m - 1, val);
+    return dicho_rec_croissant(t, m + 1, fin, val);
+}
+
+int main(void) {
+    int n, val;
+
+    printf("Donner N : ");
+    scanf("%d", &n);
+
+    int t[n];
+    printf("Entrer un tableau TRIE CROISSANT :\n");
+    for (int i = 0; i < n; i++) {
+        printf("t[%d] = ", i);
+        scanf("%d", &t[i]);
+    }
+
+    printf("Valeur a rechercher : ");
+    scanf("%d", &val);
+
+    int pos = dicho_rec_croissant(t, 0, n - 1, val);
+
+    if (pos == -1)
+        printf("Valeur %d non trouvee.\n", val);
+    else
+        printf("Valeur %d trouvee a l'index %d.\n", val, pos);
+
+    return 0;
+}
