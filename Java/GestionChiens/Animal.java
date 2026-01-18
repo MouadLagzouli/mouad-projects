cb0b9f1fdaff488cce527b45a0d8a043c305fa23
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-01-18 13:08:21 +0100

    Create Animal class implementing Affichable interface
    Représente un animal générique avec un nom. Sert de base pour l’héritage.

---
+++ Java/GestionChiens/Animal.java
@@ -0,0 +1,20 @@
+public class Animal implements Affichable {
+    protected String nom;
+
+    public Animal(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String afficher() {
+        return "Animal: " + nom;
+    }
+}
