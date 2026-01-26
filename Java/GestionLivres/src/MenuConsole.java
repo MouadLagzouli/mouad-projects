ba4f77c7e8c42467ca03b2c71d931fc79622c651
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-01-26 17:46:42 +0100

    Implement menu start method in MenuConsole
    Added a method to start the console menu and handle user choices.

--- Java/GestionLivres/src/MenuConsole.java
+++ Java/GestionLivres/src/MenuConsole.java
@@ -9,3 +9,22 @@ public class MenuConsole {
         catalogue = new Catalogue();
         sc = new Scanner(System.in);
     }
+ public void demarrer() {
+        int choix;
+        do {
+            afficherMenu();
+            choix = lireEntier("Votre choix: ");
+
+            switch (choix) {
+                case 1: ajouterLivre(); break;
+                case 2: supprimerLivre(); break;
+                case 3: chercherParISBN(); break;
+                case 4: chercherParAuteur(); break;
+                case 5: chercherParTitre(); break;
+                case 6: afficherTous(); break;
+                case 0: System.out.println("Au revoir !"); break;
+                default: System.out.println("Choix invalide.");
+            }
+            System.out.println();
+        } while (choix != 0);
+    }
