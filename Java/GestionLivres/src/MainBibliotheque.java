ede8e8c7df6f0f68fc0e7a595f63f42f02d61df1
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-01-29 19:51:28 +0100

    Update MainBibliotheque.java
    Point d’entrée du programme. Lance le menu console.

--- Java/GestionLivres/src/MainBibliotheque.java
+++ Java/GestionLivres/src/MainBibliotheque.java
@@ -1,7 +1,7 @@
 
 public class MainBibliotheque {
     public static void main(String[] args) {
-        MenuConsole menu = new MenuConsole();
+        MenuConsole menu = new MenuConsole(); 
         menu.demarrer();
     }
 }
