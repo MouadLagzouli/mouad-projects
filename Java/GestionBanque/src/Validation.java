5abcf7fd5ce11be2dd30fc50d542bf841d8aee1a
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-01 13:29:25 +0100

    Add verifierNonVide method to validate non-empty strings

--- Java/GestionBanque/src/Validation.java
+++ Java/GestionBanque/src/Validation.java
@@ -4,3 +4,6 @@ public class Validation {
     public static boolean estVide(String s) {
         return s == null || s.trim().isEmpty();
     }
+public static void verifierNonVide(String s, String msg) {
+        if (estVide(s)) throw new IllegalArgumentException(msg);
+    }
