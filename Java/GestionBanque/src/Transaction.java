629a82d80ed03ef928940aea6abb75bd130b007e
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-03 18:12:46 +0100

    Update Transaction.java

--- Java/GestionBanque/src/Transaction.java
+++ Java/GestionBanque/src/Transaction.java
@@ -2,3 +2,16 @@
 public class Transaction implements Affichable {
     private TypeTransaction type;
     private double montant;
+public Transaction(TypeTransaction type, double montant) {
+        this.type = type;
+        this.montant = montant;
+    }
+
+    public TypeTransaction getType() { return type; }
+    public double getMontant() { return montant; }
+
+    @Override
+    public String afficher() {
+        return "Transaction: " + type + " | Montant: " + montant;
+    }
+}
