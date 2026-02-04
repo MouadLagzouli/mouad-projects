d3799c3ec5cc8c2331a2aae3c15c98547a231bf5
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-04 13:56:59 +0100

    Update Compte.java

--- Java/GestionBanque/src/Compte.java
+++ Java/GestionBanque/src/Compte.java
@@ -1,1 +1,8 @@
 
+import java.util.ArrayList;
+
+public class Compte implements Affichable {
+    private String nom;
+    private String numCompte;
+    private double solde;
+    private ArrayList<Transaction> historique;
