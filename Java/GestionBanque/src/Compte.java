c4beff3d5a578927fea4b86ac63c614a83728990
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-04 13:59:41 +0100

    Update Compte.java

--- Java/GestionBanque/src/Compte.java
+++ Java/GestionBanque/src/Compte.java
@@ -6,3 +6,12 @@ public class Compte implements Affichable {
     private String numCompte;
     private double solde;
     private ArrayList<Transaction> historique;
+ public Compte(String nom, String numCompte, double soldeInitial) {
+        Validation.verifierNonVide(nom, "Le nom ne doit pas être vide.");
+        Validation.verifierNonVide(numCompte, "Le numéro de compte ne doit pas être vide.");
+
+        this.nom = nom.trim();
+        this.numCompte = numCompte.trim();
+        this.solde = soldeInitial;
+        this.historique = new ArrayList<>();
+    }
