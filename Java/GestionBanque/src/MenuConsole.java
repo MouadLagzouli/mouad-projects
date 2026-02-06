1277a3f154dc40576ebbebe3172141919db1291b
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-06 16:44:02 +0100

    Update MenuConsole.java

--- Java/GestionBanque/src/MenuConsole.java
+++ Java/GestionBanque/src/MenuConsole.java
@@ -9,3 +9,23 @@ public class MenuConsole {
         banque = new Banque();
         sc = new Scanner(System.in);
     }
+public void demarrer() {
+        int choix;
+        do {
+            afficherMenu();
+            choix = lireEntier("Votre choix: ");
+
+            switch (choix) {
+                case 1: creerCompte(); break;
+                case 2: supprimerCompte(); break;
+                case 3: deposer(); break;
+                case 4: retirer(); break;
+                case 5: chercherParNumero(); break;
+                case 6: chercherParNom(); break;
+                case 7: afficherTout(); break;
+                case 8: afficherHistorique(); break;
+                case 0: System.out.println("Au revoir !"); break;
+                default: System.out.println("Choix invalide.");
+            }
+
+            
