cab23dfc8bd6c6838af4b95fc9c64de166591385
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-07 14:39:49 +0100

    Update MenuConsole.java

--- Java/GestionBanque/src/MenuConsole.java
+++ Java/GestionBanque/src/MenuConsole.java
@@ -27,5 +27,20 @@ public void demarrer() {
                 case 0: System.out.println("Au revoir !"); break;
                 default: System.out.println("Choix invalide.");
             }
+System.out.println();
+        } while (choix != 0);
+    }
 
+    private void afficherMenu() {
+        System.out.println("==== MENU BANQUE ====");
+        System.out.println("1) Créer un compte");
+        System.out.println("2) Supprimer un compte (par numéro)");
+        System.out.println("3) Déposer");
+        System.out.println("4) Retirer");
+        System.out.println("5) Chercher un compte (par numéro)");
+        System.out.println("6) Chercher des comptes (par nom)");
+        System.out.println("7) Afficher tous les comptes");
+        System.out.println("8) Afficher historique d’un compte");
+        System.out.println("0) Quitter");
+    }
             
