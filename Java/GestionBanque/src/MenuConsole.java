45f407b8d8a80f293ad5f341908dfb8502bf5204
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-02-18 01:17:31 +0000

    Fix formatting in MenuConsole.java

--- Java/GestionBanque/src/MenuConsole.java
+++ Java/GestionBanque/src/MenuConsole.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MenuConsole {
    private Banque banque;                    
     private Scanner sc;
-
+    
     public MenuConsole() {
         banque = new Banque();
         sc = new Scanner(System.in);
