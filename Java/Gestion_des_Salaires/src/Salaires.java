5501e1c4989969ce4f406c9783499ded242b2c7e
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-03-07 01:44:09 +0000

    Update Salaires.java

--- Java/Gestion_des_Salaires/src/Salaires.java
+++ Java/Gestion_des_Salaires/src/Salaires.java
@@ -5,10 +5,10 @@ public class Salaires {
 
         p.ajouterEmploye(new Vendeur("Sami", "Business", 45, "1995", 30000));
         p.ajouterEmploye(new Representant("Amine", "Vendtout", 25, "2001", 20000));
-        p.ajouterEmploye(new Technicien("Ali", "Bosseur", 28, "1998", 1000));
+        p.ajouterEmploye(new Technicien("Ahmed", "Bosseur", 28, "1998", 1000));
         p.ajouterEmploye(new Manutentionnaire("Saad", "Stocketout", 32, "1998", 45));
         p.ajouterEmploye(new TechnARisque("Islam", "Flippe", 28, "2000", 1000));
-        p.ajouterEmploye(new ManutARisque("Zaid", "Abordage", 30, "2001", 45));
+        p.ajouterEmploye(new ManutARisque("Mouad", "Abordage", 30, "2001", 45));
 
         p.afficherSalaires();
         System.out.println("Le salaire moyen dans l'entreprise est de " + p.salaireMoyen() + " DH");
