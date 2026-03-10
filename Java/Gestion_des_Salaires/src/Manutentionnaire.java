e468b185886724cea39effeafa8655bd3fe26d3b
MOUAD LAGZOULI <160407938+MouadLagzouli@users.noreply.github.com>
2026-03-10 02:26:46 +0000

    Update Manutentionnaire.java

--- Java/Gestion_des_Salaires/src/Manutentionnaire.java
+++ Java/Gestion_des_Salaires/src/Manutentionnaire.java
@@ -4,15 +4,15 @@ public class Manutentionnaire extends Employe {
     public Manutentionnaire(String prenom, String nom, int age, String dateEntree, int nbHeuresMensuelles) {
         super(prenom, nom, age, dateEntree);
         this.nbHeuresMensuelles = nbHeuresMensuelles;
-    }
+      }
 
     @Override
     public double calculerSalaire()  {
         return nbHeuresMensuelles * 65.0;
-    }
+      }
 
     @Override
     public String getNom()  {
         return "Le manut. " + Prenom + "   " + nom;
+       }
     }
-}
